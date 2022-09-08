bd4ebc46963c52da7b6f668445e69d2c7d90b76b
Maksim <lost@MacBook-Pro-Maksim.local>
2022-09-09 00:23:50 +0300

    done hw

---
+++ 1_sem/1_task.py
@@ -0,0 +1,18 @@
+#Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+day = input()
+if day == "1":
+    print("no")
+if day == "2":
+    print("no")
+if day == "3":
+    print("no")
+if day == "4":
+    print("no")
+if day == "5":
+    print("no")
+if day == "6":
+    print("YES")
+if day == "7":
+    print("YEEEES")
+
